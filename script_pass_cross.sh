8ea83f17e3cdab1b6a51eaef898b1f4f9d400f50
Giuliano Sohrobigarat <gino.sohro@yahoo.com.ar>
2024-11-07 11:52:52 -0300

    Update script_pass_cross.sh

--- script_pass_cross.sh
+++ script_pass_cross.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+#!/bin/bash
+#Nombrar las maquinas en el host
+echo "192.168.56.4 testing" | sudo tee -a /etc/hosts
+echo "192.168.56.5 production" | sudo tee -a /etc/hosts
+
+#Remover contraseña al usar sudo
+if sudo grep -q "^vagrant ALL=(ALL) NOPASSWD:ALL" /etc/sudoers; then
+    echo "La configuración de sudo sin contraseña ya está aplicada."
+else
+    echo "Agregando configuración de sudo sin contraseña para el usuario vagrant..."
+    echo "vagrant ALL=(ALL) NOPASSWD:ALL" | sudo EDITOR='tee -a' visudo
+fi
+
 sudo dnf -y install sshpass
 VM1_IP="192.168.56.4"     # IP de la primera máquina virtual
 VM2_IP="192.168.56.5"     # IP de la segunda máquina virtual
